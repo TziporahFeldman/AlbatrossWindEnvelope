f9a4b8afb7bb236e5730d41dd817caadfb137b94
tfeldmanSBU <tziporah.feldman@stonybrook.edu>
2021-08-12 11:51:24 -0400

    Comparing MERRA2 and ERA5 for 2019 data

--- extract_wind_to_latlonMERRA-2.R
+++ extract_wind_to_latlonMERRA-2.R
@@ -29,11 +29,68 @@ require(RColorBrewer) # color palette
 library(viridis)# color palette
 library(maps)# world maps for ggplot
 
+##########################################################################################
+# AUXILIARY FUNCTIONS
+# run before analyses to calculate wind speed 
+##########################################################################################
+# -----------------------------------------------------------------
+# uv2ddff
+# -----------------------------------------------------------------
+uv2ddff <- function(u, v = NULL, rad = FALSE){
+  # if input u is zoo or data.frame
+  zoo_index <- NULL # Default
+  if (inherits(u, c("zoo", "data.frame"))) {
+    # If input 'u' is zoo: keep index
+    if (inherits(u, "zoo")) zoo_index <- index(u)
+    if (!all(c("u", "v") %in% names(u)))
+      stop("necessary colums \"u\" and/or \"v\" missing")
+    # If "v" is set in addition: warn
+    if (!is.null(v)) {
+      warning(sprintf("input \"u\" to uv2ddff is \"%s\":", class(u)),
+              "\"v\" specified as well but will be ignored!")
+    }
+    v = as.numeric(u$v)
+    u = as.numeric(u$u)
+    # if u has 2 columns the second column is taken as v
+  } else if (NCOL(u) == 2) {
+    v <- u[,2]
+    u <- u[,1]
+  } else {
+    if (is.null(v)) stop("input \"v\" missing")
+    # If lenths do not match and none of them is of length 1: stop.
+    if (!(length(u) == length(v)) & !any(c(length(u), length(v)) == 1L)) {
+      stop("Length of \"u\" and \"v\" not identical")
+      # Else recycle the one with length one to the length of the other one.
+      # Damn it, so much one's in one sentence!
+    } else if (length(u) == 1) {
+      u <- rep(u, length(v))
+    } else if (length(v) == 1) {
+      v <- rep(v, length(u))
+    }
+  }
+  # polar coordinates:
+  ff <- sqrt(u^2 + v^2)
+  dd <- atan(v/u) + (u < 0) * pi
+  # Only non-na combis
+  idx <- which(!is.na(dd) & !is.na(ff));   dd[idx] <- dd[idx] + 2 * pi
+  # convert angle to meteorological convention
+  dd <- 3 * pi / 2 - dd
+  idx <- which(!is.na(dd) & !is.na(ff));   dd[idx] <- dd[idx] + 2 * pi
+  # if rad (radiants) = F we have to convert to degrees.
+  if (!rad) dd <- dd * 180 / pi
+  res <- data.frame(dd, ff)
+  if (is.null(zoo_index)) return(res) else return(zoo(res, zoo_index))
+}
+
+wrap360 = function(lon) {lon360<-ifelse(lon<0,lon+360,lon);return(lon360)}
+Lon360to180 <- function(lon){
+  ((lon + 180) %% 360) - 180
+}
+
 #################################################################################
 # Download and Import data
 #################################################################################
-# -80, -70, -20, -30 Extent
-
+# -80, -70, -20, -30 Extent 
 # ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
 #  Useful Links
 # ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
@@ -43,8 +100,8 @@ library(maps)# world maps for ggplot
 # Hourly Data: https://disc.gsfc.nasa.gov/datasets/M2I1NXASM_5.12.4/summary
 
 
-setwd("/Users/tziporahserota/Desktop/Merra-2Datasets/Nov-Jan2019_hourly/")
-a <- list.files("/Users/tziporahserota/Desktop/Merra-2Datasets/Nov-Jan2019_hourly/", pattern = ".nc4")
+setwd("/Users/tziporahserota/Desktop/Thorne Lab/Analyses/Wind/Datasets/Merra-2Datasets/Nov2019-Jan2020_hourly/")
+a <- list.files("/Users/tziporahserota/Desktop/Thorne Lab/Analyses/Wind/Datasets/Merra-2Datasets/Nov2019-Jan2020_hourly/", pattern = ".nc4")
 
 # ---------- ---------- ---------- ---------- ---------- ---------- ---------- ----------
 #  Create list of times
@@ -217,7 +274,7 @@ for ( j in 1:length(m$id)) {
 spvec <- substr(m$id,1,4)
 m<-m %>% mutate(species=spvec)
 
-write_csv(m, '/Users/tziporahserota/Desktop/Thorne Lab/Analyses/Wind/wind_paired-data/2019-2020/hourly/allbirds_hourly.csv')
+write_csv(m, '/Volumes/GoogleDrive/.shortcut-targets-by-id/1-mLOKt79AsOpkCFrunvcUj54nuqPInxf/THORNE_LAB/Data!/Feldman_Analysis/Wind/wind_paired-data/2019-2020/hourly/allbirds_hourly.csv')
 
 
 ####################################################################################################
@@ -250,7 +307,11 @@ m_hi$U10M<- NA
 m_hi$V10M<- NA
 m_hi$wind_speed2M <- NA
 m_hi$wind_speed10M <- NA
+m_hi$wind_dir3602M <- NA
+m_hi$wind_dir36010M <- NA
 m_hi$windshear <- NA
+m_hi$bwa<- NA
+m_hi$bwa_class<- NA
 
 # -----------------------------------------------------------------------------
 # Add Bird Speed, Distance, Bearing
@@ -277,6 +338,7 @@ for (i in 1:length(trips)) {
 
 # Import hourly lat lon with wind u and v
 m_lo <- m
+# m_lo <- read.csv('/Volumes/GoogleDrive/.shortcut-targets-by-id/1-mLOKt79AsOpkCFrunvcUj54nuqPInxf/THORNE_LAB/Data!/Feldman_Analysis/Wind/wind_paired-data/2019-2020/hourly/allbirds_hourly.csv')
 birds<-unique(m_lo$id)
 
 for (i in 1:length(birds)) {
@@ -310,18 +372,44 @@ for (i in 1:length(birds)) {
     # uv2ddff for wind direction and velocity
     ddff2M <- uv2ddff(mi_hi$U2M[j],mi_hi$V2M[j])
     mi_hi$wind_speed2M[j] <- ddff2M$ff
+    mi_hi$wind_dir3602M[j]<-ddff2M$dd
     
     ddff10M <- uv2ddff(mi_hi$U10M[j],mi_hi$V10M[j])
     mi_hi$wind_speed10M[j] <- ddff10M$ff
+    mi_hi$wind_dir36010M[j]<-ddff10M$dd
     
     mi_hi$windshear[j]<-  mi_hi$wind_speed10M[j]- mi_hi$wind_speed2M[j]
+    
+    # bird-wind-angle
+    bird_wind_angle<-abs(Lon360to180(mi_hi$bearingij[j]-mi_hi$wind_dir3602M[j]))
+    
+    # bwa class
+    # Definitions from Spear and Ainley 1997
+    # Headwind  = abs(bird-wind): 0-59 
+    # Crosswind = abs(bird-wind): 60-119 
+    # Tailwind =  abs(bird-wind): 120-180 
+    if (is.na(bird_wind_angle)) {
+      mi_hi$bwa_class[j] <- NA
+    }else{
+      if (bird_wind_angle < 50) {
+        mi_hi$bwa_class[j] <- "Head-Wind"
+      }else if (bird_wind_angle > 130) {
+        mi_hi$bwa_class[j] <- "Tail-Wind"
+      }else{
+        mi_hi$bwa_class[j] <- "Cross-Wind"
+      }
+    }
+    
+    mi_hi$bwa[j] <- bird_wind_angle
+  
   }
+  
   spvec<-substr(birdi, 1, 4)
   mi_hi$species<-spvec
   
   
   # write individual bird file
-  dir_i<-paste0('/Users/tziporahserota/Desktop/Thorne Lab/Analyses/Wind/wind_paired-data/2019-2020/indiv_birds/', birdi, '_wind_bwa.csv')
+  dir_i<-paste0('/Volumes/GoogleDrive/.shortcut-targets-by-id/1-mLOKt79AsOpkCFrunvcUj54nuqPInxf/THORNE_LAB/Data/Feldman_Analysis/Wind/wind_paired-data/2019-2020/indiv_birds/', birdi, '_wind_bwa.csv')
   write_csv(mi_hi, dir_i)
   
   rm(list=ls()[! ls() %in% c("wrap360", "uv2ddff", "Lon360to180", "birds", "m_lo", "m_hi", "i", "int_now")])
